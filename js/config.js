e0289e0dfb320e4b9fa37ec8edae219748c82e42
zvtx <49553428+zvtx@users.noreply.github.com>
2025-07-12 15:19:10 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -154,7 +154,7 @@ const API_SITES = {
     //     adult: true
     // },
      huangcang: {
-         api: 'https://hsckzy.vip',
+         api: 'https://hsckzy888.com',
         name: '黄色仓库',
         adult: true,
         detail: 'https://hsckzy.vip'
